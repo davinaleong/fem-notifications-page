f8e79d80f23b01e1635df40331c2161458ec1039
Davina Leong <davina.leong.sy@gmail.com>
2022-12-23 17:54:20 +0800

    WIP populating data

---
+++ scripts/data.js
@@ -0,0 +1,61 @@
+const profiles = {
+  markWebber: {
+    name: "Mark Webber",
+    avatar: "./../images/avatar-mark-webber.webp",
+  },
+  angelaGray: {
+    name: "Angela Gray",
+    avatar: "./../images/avatar-angela-gray.webp",
+  },
+  jacobThompson: {
+    name: "Jacob Thompson",
+    avatar: "./../images/avatar-jacob-thompson.webp",
+  },
+  rizkyHasanuddin: {
+    name: "Rizky Hasanuddin",
+    avatar: "./../images/avatar-rizky-hasanuddin.webp",
+  },
+  kimberlySmith: {
+    name: "Kimberly Smith",
+    avatar: "./../images/avatar-kimberly-smith.webp",
+  },
+  nathanPeterson: {
+    name: "Nathan Peterson",
+    avatar: "./../images/avatar-nathan-peterson.webp",
+  },
+  annaKim: {
+    name: "Anna Kim",
+    avatar: "./../images/avatar-anna-kim.webp",
+  },
+}
+
+const notificationTypes = {
+  reaction: {
+    name: "Reaction",
+    message: "reacted to your recent post",
+  },
+  following: {
+    name: "Following",
+    message: "followed you",
+  },
+  joinedGroup: {
+    name: "Joined Group",
+    message: "has joined your group",
+  },
+  privateMessage: {
+    name: "Private Message",
+    message: "sent you a private message",
+  },
+  comment: {
+    name: "Comment",
+    message: "commented on your picture",
+  },
+  leftGroup: {
+    name: "Left Group",
+    message: "left the group",
+  },
+}
+
+const notifications = [
+  //
+]
